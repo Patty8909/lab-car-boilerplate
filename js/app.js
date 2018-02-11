0216b14c80e474fcd63cb5efd881b862bcf818f6
patty8909 <patty90_30@hotmail.com>
2018-02-11 10:14:40 -0500

    añade maps y estilos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // La funcionalidad de tu proyecto
+
+
+function initMap() {
+    var uluru = {lat: -12.0253234, lng: -77.1194875};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
+      
