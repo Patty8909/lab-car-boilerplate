d887a67a8a7300ea99e45c55954f9bacdd64646c
patty8909 <patty90_30@hotmail.com>
2018-02-11 21:57:57 -0500

    modifica readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lab-<ar
+
+Ésta es una página web responsive (versión desktop y mobile), que contiene en su versión desktop, tres ventanas emergentes (modals) diferentes, el primero es para registrarse, el segundo es para iniciar sesión y el tercero un formulario para convertirse en conductor.
+
+Además contiene un mapa de google que incluye un marcador para indicar una ubicación específica.
+
+## Versión Desktop:
+
+![Sin titulo](assets/images/v-desktop.png)
--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-// La funcionalidad de tu proyecto
-
-
+// funcionalidad google maps
 function initMap() {
-    var uluru = {lat: -12.0253234, lng: -77.1194875};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+  var uluru = {
+    lat: -12.0253234,
+    lng: -77.1194875};
+ 
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
       
